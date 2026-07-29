716bdd27f6e2d4f2e540a09affb8e0c1b6e76410
quenashleymashamaite01 <241913309@tut4life.ac.za>
2026-07-29 21:58:46 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author Que27
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declare variable
+        MyFirstFrame  myFrame;
+        
+        //Instantiate the frame
+        myFrame = new MyFirstFrame();
+        
+        
+        
+        
+        
+    }
+    
+}
